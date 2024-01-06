8fe6e31682c49a7e0335d9855a7a765a97d251e4
ljm <yhxljm@qq.com>
2024-01-06 13:50:07 +0800

    新增视频解码demo,decode_video

--- 7-1-ffmpeg-demuxer/07-6-decode_video.h
+++ 7-1-ffmpeg-demuxer/07-6-decode_video.h
@@ -12,12 +12,11 @@
 
 #include <libavutil/frame.h>
 #include <libavutil/mem.h>
-#include <libavutil/frame.h>
 
 #include <libavcodec/avcodec.h>
 
-#define AUDIO_INBUF_SIZE 20480
-#define AUDIO_REFILL_THRESH 4096
+#define VIDEO_INBUF_SIZE 20480
+#define VIDEO_REFILL_THRESH 4096
 #pragma warning(disable : 4996)
 
 static char err_buf[128] = { 0 };
@@ -27,18 +26,17 @@ static char* av_get_err(int errnum)
     return err_buf;
 }
 
-static void print_sample_format(const AVFrame* frame)
+static void print_video_format(const AVFrame* frame)
 {
-    printf("ar-samplerate: %uHz\n", frame->sample_rate);
-    printf("ac-channel: %u\n", frame->channels);
-    printf("f-format: %u\n", frame->format);// 格式需要注意，实际存储到本地文件时已经改成交错模式
+    printf("width: %u\n", frame->width);
+    printf("height: %u\n", frame->height);
+    printf("format: %u\n", frame->format);// 格式需要注意
 }
 
 static void decode(AVCodecContext* dec_ctx, AVPacket* pkt, AVFrame* frame,
     FILE* outfile)
 {
-    int i, ch;
-    int ret, per_sample_szie;
+    int ret;
     /* send the packet with the compressed data to the decoder */
     ret = avcodec_send_packet(dec_ctx, pkt);
     if (ret == AVERROR(EAGAIN))
@@ -49,54 +47,55 @@ static void decode(AVCodecContext* dec_ctx, AVPacket* pkt, AVFrame* frame,
     {
         fprintf(stderr, "Error submitting the packet to the decoder, err:%s, pkt_size:%d\n",
             av_get_err(ret), pkt->size);
-        //        exit(1);
         return;
     }
 
     /* read all the output frames (infile general there may be any number of them */
     while (ret >= 0)
     {
-        // 对于frame, avcodec_receive_frame内部每次都先调用 av_frame_unref
+        // 对于frame, avcodec_receive_frame内部每次都先调用
         ret = avcodec_receive_frame(dec_ctx, frame);
-        if (ret == AVERROR(EAGAIN) || ret == AVERROR_EOF)//EAGAIN 需要更多数据才能输出帧， AVERROR_EOF 所有剩余的帧都已经输出，没有更多的帧可供输出了 
+        if (ret == AVERROR(EAGAIN) || ret == AVERROR_EOF)
             return;
         else if (ret < 0)
         {
             fprintf(stderr, "Error during decoding\n");
             exit(1);
         }
-        per_sample_szie = av_get_bytes_per_sample(dec_ctx->sample_fmt);
-        if (per_sample_szie < 0)
-        {
-            /* This should not occur, checking just for paranoia */
-            fprintf(stderr, "Failed to calculate data size\n");
-            exit(1);
-        }
         static int s_print_format = 0;
         if (s_print_format == 0)
         {
             s_print_format = 1;
-            print_sample_format(frame);
+            print_video_format(frame);
         }
-        /**
-            P表示Planar（平面），其数据格式排列方式为 :
-            LLLLLLRRRRRRLLLLLLRRRRRRLLLLLLRRRRRRL...（每个LLLLLLRRRRRR为一个音频帧）
-            而不带P的数据格式（即交错排列）排列方式为：
-            LRLRLRLRLRLRLRLRLRLRLRLRLRLRLRLRLRLRL...（每个LR为一个音频样本）
-            对应：AV_SAMPLE_FMT_FLTP
-         播放范例：   ffplay -ar 48000 -ac 2 -f f32le believe.pcm
-          */
-        int datasize=frame->nb_samples* frame->channels * per_sample_szie;
-        printf("this frame datasize:%d\n", datasize);
-        for (i = 0; i < frame->nb_samples; i++)
-        {
-            for (ch = 0; ch < dec_ctx->channels; ch++)  // 交错的方式写入, 大部分float的格式输出
-                fwrite(frame->data[ch] + per_sample_szie * i, 1, per_sample_szie, outfile);
-        }
+
+        // 一般H264默认为 AV_PIX_FMT_YUV420P, 具体怎么强制转为 AV_PIX_FMT_YUV420P 在音视频合成输出的时候讲解
+        // frame->linesize[1]  对齐的问题
+        // 正确写法  linesize[]代表每行的字节数量，所以每行的偏移是linesize[]
+        for (int j = 0; j < frame->height; j++)
+            fwrite(frame->data[0] + j * frame->linesize[0], 1, frame->width, outfile);
+        for (int j = 0; j < frame->height / 2; j++)
+            fwrite(frame->data[1] + j * frame->linesize[1], 1, frame->width / 2, outfile);
+        for (int j = 0; j < frame->height / 2; j++)
+            fwrite(frame->data[2] + j * frame->linesize[2], 1, frame->width / 2, outfile);
+
+        // 错误写法 用source.200kbps.766x322_10s.h264测试时可以看出该种方法是错误的
+        //  写入y分量
+//        fwrite(frame->data[0], 1, frame->width * frame->height,  outfile);//Y
+//        // 写入u分量
+//        fwrite(frame->data[1], 1, (frame->width) *(frame->height)/4,outfile);//U:宽高均是Y的一半
+//        //  写入v分量
+//        fwrite(frame->data[2], 1, (frame->width) *(frame->height)/4,outfile);//V：宽高均是Y的一半
     }
 }
-// 播放范例：   ffplay -ar 48000 -ac 2 -f f32le believe.pcm
-int decode_audio(int argc, char** argv)
+
+
+// 注册测试的时候不同分辨率的问题
+// 提取H264: ffmpeg -i source.200kbps.768x320_10s.flv -vcodec libx264 -an -f h264 source.200kbps.768x320_10s.h264
+// 提取MPEG2: ffmpeg -i source.200kbps.768x320_10s.flv -vcodec mpeg2video -an -f mpeg2video source.200kbps.768x320_10s.mpeg2
+// 播放：ffplay -pixel_format yuv420p -video_size 512x288 -framerate 25  out.yuv
+// 错误的播放方式 会导致花屏，数据错乱 ffplay -pixel_format yuv420p -video_size 768x320 -framerate 25  out.yuv
+int decode_video(int argc, char** argv)
 {
     const char* outfilename;
     const char* filename;
@@ -107,40 +106,40 @@ int decode_audio(int argc, char** argv)
     int ret = 0;
     FILE* infile = NULL;
     FILE* outfile = NULL;
-    uint8_t inbuf[AUDIO_INBUF_SIZE + AV_INPUT_BUFFER_PADDING_SIZE];
-    uint8_t* data = NULL;//准备解析的数据
-    size_t   data_size = 0;//准备解析的数据大小
+    // AV_INPUT_BUFFER_PADDING_SIZE 在输入比特流结尾的要求附加分配字节的数量上进行解码
+    uint8_t inbuf[VIDEO_INBUF_SIZE + AV_INPUT_BUFFER_PADDING_SIZE];
+    uint8_t* data = NULL;
+    size_t   data_size = 0;
     AVPacket* pkt = NULL;
     AVFrame* decoded_frame = NULL;
 
     if (argc <= 2)
     {
-        //fprintf(stderr, "Usage: %s <input file> <output file>\n", argv[0]);
-        //exit(0);
-        filename = "..\\res\\believe.mp3";
-        outfilename = "..\\res\\believe.pcm";
-    }
-    else {
+        fprintf(stderr, "Usage: %s <input file> <output file>\n", argv[0]);
+        filename = "..\\res\\test.h264";
+        outfilename = "..\\res\\out.yuv";
+    }else{
         filename = argv[1];
         outfilename = argv[2];
-}
-    pkt = av_packet_alloc();
-    enum AVCodecID audio_codec_id = AV_CODEC_ID_AAC;
-    if (strstr(filename, "aac") != NULL)
-    {
-        audio_codec_id = AV_CODEC_ID_AAC;
     }
-    else if (strstr(filename, "mp3") != NULL)
+
+    pkt = av_packet_alloc();
+    enum AVCodecID video_codec_id = AV_CODEC_ID_H264;
+    if (strstr(filename, "264") != NULL)
     {
-        audio_codec_id = AV_CODEC_ID_MP3;
+        video_codec_id = AV_CODEC_ID_H264;
+    }
+    else if (strstr(filename, "mpeg2") != NULL)
+    {
+        video_codec_id = AV_CODEC_ID_MPEG2VIDEO;
     }
     else
     {
-        printf("default codec id:%d\n", audio_codec_id);
+        printf("default codec id:%d\n", video_codec_id);
     }
 
     // 查找解码器
-    codec = avcodec_find_decoder(audio_codec_id);  // AV_CODEC_ID_AAC
+    codec = avcodec_find_decoder(video_codec_id);  // AV_CODEC_ID_H264
     if (!codec) {
         fprintf(stderr, "Codec not found\n");
         exit(1);
@@ -179,7 +178,7 @@ int decode_audio(int argc, char** argv)
 
     // 读取文件进行解码
     data = inbuf;
-    data_size = fread(inbuf, 1, AUDIO_INBUF_SIZE, infile);
+    data_size = fread(inbuf, 1, VIDEO_INBUF_SIZE, infile);
 
     while (data_size > 0)
     {
@@ -191,8 +190,7 @@ int decode_audio(int argc, char** argv)
                 exit(1);
             }
         }
-        //会根据解码器的类型，解析出一个完整的packet，核心就是确定data中下一个帧的边界，
-        //此时的pkt仅仅data和size有值，其他的都是0 ，且第一次调用时pkt->data == data（ 不会生成新的内存），pkt.size就是第一帧的大小
+
         ret = av_parser_parse2(parser, codec_ctx, &pkt->data, &pkt->size,
             data, data_size,
             AV_NOPTS_VALUE, AV_NOPTS_VALUE, 0);
@@ -207,21 +205,20 @@ int decode_audio(int argc, char** argv)
         if (pkt->size)
             decode(codec_ctx, pkt, decoded_frame, outfile);
 
-        if (data_size < AUDIO_REFILL_THRESH)    // 如果数据少了则再次读取
+        if (data_size < VIDEO_REFILL_THRESH)    // 如果数据少了则再次读取
         {
             memmove(inbuf, data, data_size);    // 把之前剩的数据拷贝到buffer的起始位置
             data = inbuf;
-            // 读取数据 长度: AUDIO_INBUF_SIZE - data_size ,确保总长度仍然是AUDIO_INBUF_SIZE
-            len = fread(data + data_size, 1, AUDIO_INBUF_SIZE - data_size, infile);
+            // 读取数据 长度: VIDEO_INBUF_SIZE - data_size
+            len = fread(data + data_size, 1, VIDEO_INBUF_SIZE - data_size, infile);
             if (len > 0)
                 data_size += len;
         }
     }
 
-    /* 冲刷解码器  ，比喻：碗底残留 */
+    /* 冲刷解码器 */
     pkt->data = NULL;   // 让其进入drain mode
     pkt->size = 0;
-    //解码器中可能还有一些数据，需要通过输入一个空数据包，它会告诉解码器没有更多的输入数据了，解码器应该开始输出所有剩余的帧
     decode(codec_ctx, pkt, decoded_frame, outfile);
 
     fclose(outfile);
