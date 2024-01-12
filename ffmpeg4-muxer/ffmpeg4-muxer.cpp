6c4e6065f6770c02db78c916dc1584af4ef7a294
ljm <yhxljm@qq.com>
2024-01-12 15:21:47 +0800

    新增ffmpeg4.2.1版本，新增muxing_flv代码，手动生成pcm,yuv数据合成为flv文件

---
+++ ffmpeg4-muxer/ffmpeg4-muxer.cpp
@@ -0,0 +1,11 @@
+﻿// ffmpeg4-muxer.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+#include "08-03-muxing_flv-ffmpeg4x.h"
+
+int main(int argc,char** argv)
+{
+    muxing_flv_ffmpeg4(argc, argv);
+    std::cout << "Hello World!\n";
+}
